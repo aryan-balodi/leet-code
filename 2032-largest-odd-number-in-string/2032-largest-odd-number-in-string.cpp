cffd02dc9aa82f759b55d7e2c50249eeb8d68520
Balodi <balodiaryan5@gmail.com>
2025-08-31 23:38:49 +0530

    Time: 0 ms (100%), Space: 17.9 MB (36.29%) - LeetHub

---
+++ 2032-largest-odd-number-in-string/2032-largest-odd-number-in-string.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string largestOddNumber(string num) {
+        int n = num.length();
+        int end = -1;
+        for (int i = n - 1; i >= 0; i--) {
+            if (num[i] % 2 != 0) {
+                end = i;
+                break;
+            }
+        }
+        if (end == -1) return "";
+        else return num.substr(0, end + 1);
+
+    }
+};
