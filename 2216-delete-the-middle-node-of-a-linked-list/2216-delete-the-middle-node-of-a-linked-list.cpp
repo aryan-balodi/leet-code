4aedd393b25d4d129bba31713b33ca0abdd0239b
Balodi <balodiaryan5@gmail.com>
2025-10-08 18:11:12 +0530

    Time: 0 ms (100%), Space: 312.1 MB (18.35%) - LeetHub

--- 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
+++ 2216-delete-the-middle-node-of-a-linked-list/2216-delete-the-middle-node-of-a-linked-list.cpp
@@ -10,51 +10,69 @@
  */
 class Solution {
 public:
-    int ReturnLen(ListNode* head) {
-        ListNode *temp = head;
-        int count = 0;
+    // int ReturnLen(ListNode* head) {
+    //     ListNode *temp = head;
+    //     int count = 0;
 
-        while (temp) {
-            count++;
-            temp = temp->next;
-        }
-        return count;
-    }
+    //     while (temp) {
+    //         count++;
+    //         temp = temp->next;
+    //     }
+    //     return count;
+    // }
 
-    ListNode *DelByk(ListNode *head, int k) {
-        if (head == NULL || head->next == NULL) return NULL;
+    // ListNode *DelByk(ListNode *head, int k) {
+    //     if (head == NULL || head->next == NULL) return NULL;
 
-        if (k == 0) {
-            ListNode* temp = head;
-            head = head->next;
-            delete temp;
-            return head;
-        }
+    //     if (k == 0) {
+    //         ListNode* temp = head;
+    //         head = head->next;
+    //         delete temp;
+    //         return head;
+    //     }
 
-        ListNode *temp = head; 
-        ListNode *prev = NULL; 
-        int count = -1;
+    //     ListNode *temp = head; 
+    //     ListNode *prev = NULL; 
+    //     int count = -1;
 
-        while (temp != NULL) {
-            count++;
-            if (count == k) {
-                prev->next = temp->next;
-                delete temp;
-                break;
-            }
-            prev = temp;
-            temp = temp->next;
-        }
-        return head;
+    //     while (temp != NULL) {
+    //         count++;
+    //         if (count == k) {
+    //             prev->next = temp->next;
+    //             delete temp;
+    //             break;
+    //         }
+    //         prev = temp;
+    //         temp = temp->next;
+    //     }
+    //     return head;
 
-    }
+    // }
 
     ListNode* deleteMiddle(ListNode* head) {
-        int len = ReturnLen(head);
-        int k = len / 2;
+        // int len = ReturnLen(head);
+        // int k = len / 2;
 
-        head = DelByk(head, k);
+        // head = DelByk(head, k);
+
+        // return head;
+        
+        if (head == NULL || head->next == NULL) return NULL;
+
+        ListNode *slow = head; ListNode *fast = head;
+        ListNode *prev = head;
+
+        while (fast != nullptr && fast->next != nullptr) {
+            prev = slow;
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+        ListNode *temp = slow;
+        prev->next = slow->next;
+        delete temp;
 
         return head;
+
     }
 };
